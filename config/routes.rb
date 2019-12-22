204121888a4f2b4c791f5f46b3c3023ccc1f5238
carladgibin <carladgibin@gmail.com>
2019-12-22 18:59:36 +0000

    meditation_c

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :meditations
   root to: 'pages#home'
   get 'pages/about'
-  get 'pages/meditations'
+  get 'pages/discover_meditation'
+  get 'pages/types_of_meditation'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
